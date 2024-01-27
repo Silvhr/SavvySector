29f90e9d95a8a9a03705420fead32e55730d9e9e
Zo <zo.abbasi1@gmail.com>
2024-01-27 17:49:43 -0600

    Zo tried to merge

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ export default async function IndexPage({
 }: {
   searchParams: { q: string };
 }) {
-<<<<<<< HEAD
   // const search = searchParams.q ?? '';
   // const result = await sql`
   //   SELECT id, name, username, email 
@@ -23,20 +22,10 @@ export default async function IndexPage({
   //   WHERE name ILIKE ${'%' + search + '%'};
   // `;
   // const users = result.rows as User[];
-=======
-  const search = searchParams.q ?? '';
-//   const result = await sql`
-//     SELECT id, name, username, email 
-//     FROM users 
-//     WHERE name ILIKE ${'%' + search + '%'};
-//   `;
-//   const users = result.rows as User[];
->>>>>>> 978942e80f2e23a46012ef8ad46a338ae030ed4e
 
   return (
     <main className="p-4 md:p-1 mx-auto max-w-7xl">
       <Card className="mt-6">
-<<<<<<< HEAD
       <Title>Run Simulation</Title>
       <Card title="Timeline and Progress">
         {/* Timeline and Progress components */}
@@ -46,9 +35,6 @@ export default async function IndexPage({
         {/* Macro Information components */}
       </Card>
       {/* <Carousel /> */}
-=======
-        <UsersTable users={[]} />
->>>>>>> 978942e80f2e23a46012ef8ad46a338ae030ed4e
       </Card>
     </main>
   );
