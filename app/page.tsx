078d792f07d5628166b529834282d1078149c559
Zo <zo.abbasi1@gmail.com>
2024-01-27 19:35:18 -0600

    SP500 donut initial

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,18 @@ const financialData = [
     // ... other sectors
 ];
 
+// Example static data
+const SPData = [
+  { sector: "Technology", value: 15 },
+  { sector: "Healthcare", value: 15 },
+  { sector: "Finance", value: 30 },
+  { sector: "Materials", value: 20 },
+  { sector: "Energy", value: 30 },
+
+
+  // ... other sectors
+];
+
 export default function IndexPage() {
     return (
         <main className="p-4 md:p-1 mx-auto max-w-7xl">
@@ -21,6 +33,10 @@ export default function IndexPage() {
                 
                 <Card title="Financial Sectors Donut Chart">
                     <DonutChart data={financialData} />
+                </Card>
+
+                <Card title="S&P500 Donut Chart">
+                    <DonutChart data={SPData} />
                 </Card>
 
                 {/* More components as needed */}
