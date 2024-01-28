9f241f244614f4201d841d67ee2f39c5e1f38999
Simon Sprouse <simonhsprouse@gmail.com>
2024-01-27 23:11:41 -0600

    got the data actually working

--- app/linechart.tsx
+++ app/linechart.tsx
@@ -4,12 +4,24 @@ import React, { ReactNode, useEffect, useState } from 'react';
 
 import ReactApexChart from 'react-apexcharts';
 
+interface HistoricalData {
+    time: string;
+    value: number;
+}
+
+interface LineChartProps {
+    data: HistoricalData[];
+}
 
 interface LineChartProps {
   // Define any props your component may receive
 }
 
-const LineChart: React.FC<LineChartProps> = () => {
+const LineChart: React.FC<LineChartProps> = ({data}) => {
+
+    const times = data.map(item => item.time);
+    const series = data.map(item => item.value);
+    
   // Sample data for the chart
   const chartData = {
     options: {
@@ -17,21 +29,17 @@ const LineChart: React.FC<LineChartProps> = () => {
         id: 'basic-line',
       },
       xaxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        categories: times,
         labels: {
           show: true,  // Initially show all labels
           rotate: 0,   // Rotate labels if needed
-          formatter: function (value: string, timestamp?: number, opts?: any) {
-            // Custom formatter to show every other tick
-            return opts?.dataCategories.indexOf(value) % 2 === 0 ? value : '';
-          },
         },
       },
     },
     series: [
       {
         name: 'Series 1',
-        data: [30, 40, 35, 50, 49, 60, 70, 91, 125, 100, 80, 60],
+        data: series,
       },
     ],
   };
@@ -53,7 +61,7 @@ const LineChart: React.FC<LineChartProps> = () => {
 
   return (
     <div>
-      <h1>Hello, I'm a React component with an ApexCharts Line Chart!</h1>
+      <h1>Hello, Im a React component with an ApexCharts Line Chart!</h1>
       {renderChart && (
         <ReactApexChart
           options={chartData.options as ApexCharts.ApexOptions}
