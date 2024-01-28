0b65492a0f32ea5b117886a9987376b92e654114
Zo <zo.abbasi1@gmail.com>
2024-01-27 20:43:34 -0600

    Built out the financial data json with all sectors

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,34 @@ import DonutChart from './DonutChart'; // Ensure this path is correct
 
 // Example static data
 const financialData = [
-    { sector: "Technology", value: 25 },
-    { sector: "Healthcare", value: 15 },
-    { sector: "Finance", value: 30 },
-    // ... other sectors
+  { sector: "Communication Services", value: 0 },
+  { sector: "Consumer Discretionary", value: 0 },
+  { sector: "Consumer Staples", value: 0 },
+  { sector: "Energy", value: 30 },
+  { sector: "Finance", value: 30 },
+  { sector: "Healthcare", value: 15 },
+  { sector: "Industrials", value: 15 },
+  { sector: "Materials", value: 20 },
+  { sector: "Real Estate", value: 20 },
+  { sector: "Technology", value: 15 },
+  { sector: "Utilities", value: 15 },
+  { sector: "Cash", value: 80 },
 ];
 
 // Example static data
-const SPData = [
-  { sector: "Technology", value: 15 },
-  { sector: "Healthcare", value: 15 },
-  { sector: "Finance", value: 30 },
-  { sector: "Materials", value: 20 },
+const SPData1 = [
+  { sector: "Communication Services", value: 0 },
+  { sector: "Consumer Discretionary", value: 0 },
+  { sector: "Consumer Staples", value: 25 },
   { sector: "Energy", value: 30 },
-
+  { sector: "Finance", value: 30 },
+  { sector: "Healthcare", value: 15 },
+  { sector: "Industrials", value: 15 },
+  { sector: "Materials", value: 20 },
+  { sector: "Real Estate", value: 20 },
+  { sector: "Technology", value: 15 },
+  { sector: "Utilities", value: 15 },
+  { sector: "Cash", value: 5 },
 
   // ... other sectors
 ];
@@ -35,9 +49,9 @@ export default function IndexPage() {
                     <DonutChart data={financialData} />
                 </Card>
 
-                <Card title="S&P500 Donut Chart">
-                    <DonutChart data={SPData} />
-                </Card>
+                 <Card title="S&P500 Donut Chart">
+                    <DonutChart data={SPData1} />
+                    </Card>
 
                 {/* More components as needed */}
             </Card>
